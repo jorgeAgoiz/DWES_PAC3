e9da4e5b6c059d47728cea0d68af7f09f2cd76f9
jorgeAP <jorgeagoiz@gmail.com>
2021-04-19 15:17:56 +0200

    Empezando con la paginación

--- ListaArticulo.php
+++ ListaArticulo.php
@@ -53,6 +53,16 @@ require("./includes/header.php");
                 ?>
             </tbody>
         </table>
+        <!-- Lo dejamos por hoy en la paginación -->
+        <nav aria-label="Page navigation example">
+            <ul class="pagination">
+                <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                <li class="page-item"><a class="page-link" href="#">Next</a></li>
+            </ul>
+        </nav>
     </div>
 </div>
 
